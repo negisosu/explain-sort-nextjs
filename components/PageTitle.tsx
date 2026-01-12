c7a7d9abe023a537591915be524a511859316638
tatsu <negigi0404@gmail.com>
2026-01-12 23:46:18 +0900

    タイトル修正

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,7 +1,7 @@
 export function PageTitle() {
     return(
         <div className="py-20 md:py-32 px-10 md:px-20 w-full text-center">
-            <div className="text-2xl md:text-5xl font-bold mb-6 max-w-4xl mx-auto">ソートアルゴリズム紹介<br/>（仮タイトル）</div>
+            <div className="text-2xl md:text-5xl font-bold mb-6 max-w-4xl mx-auto">ソートアルゴリズム紹介</div>
             <p className="text-lg md:text-xl text-muted-foreground mb-8 max-w-2xl mx-auto">
                 授業で扱う基本的なソートアルゴリズムから、世界のおもしろいソートアルゴリズムを集めました。
             </p>
